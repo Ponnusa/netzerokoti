836ffee05e30b6da378332dcdfbaa7dc1f27f7f6
Ponnuswamy Saravanakumar <saravanakumar.ponnuswamy@ilmarinen.fi>
2025-07-30 14:10:00 +0300

    my working ciode

---
+++ finnish-co2-calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinnishCO2Calculator from './components/FinnishCO2Calculator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <FinnishCO2Calculator />
+    </div>
+  );
+}
+
+export default App;
